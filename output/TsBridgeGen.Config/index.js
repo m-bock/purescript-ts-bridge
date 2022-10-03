a38cec0aab7d9e58224543beefadceaadd595dbb
thought2 <no-day@posteo.net>
2022-10-03 06:01:26 +0000

    Deploying to dist from @ thought2/purescript-typescript-bridge@a74ba8c633df775cff29c390fb28cf5a109198f2 🚀

--- output/TsBridgeGen.Config/index.js
+++ output/TsBridgeGen.Config/index.js
@@ -3,39 +3,52 @@ import * as Control_Applicative from "../Control.Applicative/index.js";
 import * as Control_Bind from "../Control.Bind/index.js";
 import * as Control_Monad_Error_Class from "../Control.Monad.Error.Class/index.js";
 import * as Control_Monad_Except_Trans from "../Control.Monad.Except.Trans/index.js";
+import * as Data_Array from "../Data.Array/index.js";
 import * as Data_Bifunctor from "../Data.Bifunctor/index.js";
 import * as Data_Either from "../Data.Either/index.js";
 import * as Data_Foldable from "../Data.Foldable/index.js";
 import * as Data_Functor from "../Data.Functor/index.js";
 import * as Data_Maybe from "../Data.Maybe/index.js";
 import * as Data_Monoid from "../Data.Monoid/index.js";
+import * as Data_Newtype from "../Data.Newtype/index.js";
+import * as Data_Semigroup from "../Data.Semigroup/index.js";
 import * as Data_Show from "../Data.Show/index.js";
+import * as Data_String_CodePoints from "../Data.String.CodePoints/index.js";
+import * as Data_String_Common from "../Data.String.Common/index.js";
+import * as Data_Tuple from "../Data.Tuple/index.js";
 import * as Dodo from "../Dodo/index.js";
 import * as Effect from "../Effect/index.js";
 import * as Effect_Class from "../Effect.Class/index.js";
 import * as Effect_Class_Console from "../Effect.Class.Console/index.js";
+import * as Effect_Exception from "../Effect.Exception/index.js";
 import * as Heterogeneous_Mapping from "../Heterogeneous.Mapping/index.js";
 import * as Node_Process from "../Node.Process/index.js";
 import * as Options_Applicative_Builder from "../Options.Applicative.Builder/index.js";
 import * as Options_Applicative_Builder_Internal from "../Options.Applicative.Builder.Internal/index.js";
 import * as Options_Applicative_Extra from "../Options.Applicative.Extra/index.js";
+import * as Options_Applicative_Help_Chunk from "../Options.Applicative.Help.Chunk/index.js";
 import * as Options_Applicative_Internal_Utils from "../Options.Applicative.Internal.Utils/index.js";
 import * as Options_Applicative_Types from "../Options.Applicative.Types/index.js";
 import * as PureScript_CST from "../PureScript.CST/index.js";
 import * as Record from "../Record/index.js";
 import * as Record_Extra from "../Record.Extra/index.js";
 import * as Safe_Coerce from "../Safe.Coerce/index.js";
+import * as Text_PrettyPrint_Leijen from "../Text.PrettyPrint.Leijen/index.js";
 import * as Tidy from "../Tidy/index.js";
 import * as TsBridgeGen_Types from "../TsBridgeGen.Types/index.js";
+import * as TsBridgeGen_UIText from "../TsBridgeGen.UIText/index.js";
 import * as Type_Equality from "../Type.Equality/index.js";
 import * as Type_Proxy from "../Type.Proxy/index.js";
 import * as TypedEnv from "../TypedEnv/index.js";
+var append = /* #__PURE__ */ Data_Semigroup.append(Data_Semigroup.semigroupArray);
 var error = /* #__PURE__ */ Effect_Class_Console.error(Effect_Class.monadEffectEffect);
 var defaultFormatOptions = /* #__PURE__ */ Tidy.defaultFormatOptions(Tidy.formatErrorVoid);
-var pure = /* #__PURE__ */ Control_Applicative.pure(Effect.applicativeEffect);
 var bind = /* #__PURE__ */ Control_Bind.bind(Effect.bindEffect);
+var mapFlipped = /* #__PURE__ */ Data_Functor.mapFlipped(Effect.functorEffect);
+var show = /* #__PURE__ */ Data_Show.show(Effect_Exception.showError);
+var pure = /* #__PURE__ */ Control_Applicative.pure(Effect.applicativeEffect);
 var $$try = /* #__PURE__ */ Control_Monad_Error_Class["try"](Control_Monad_Error_Class.monadErrorEffect);
-var mapFlipped = /* #__PURE__ */ Data_Functor.mapFlipped(/* #__PURE__ */ Control_Monad_Except_Trans.functorExceptT(Effect.functorEffect));
+var mapFlipped1 = /* #__PURE__ */ Data_Functor.mapFlipped(/* #__PURE__ */ Control_Monad_Except_Trans.functorExceptT(Effect.functorEffect));
 var bind1 = /* #__PURE__ */ Control_Bind.bind(/* #__PURE__ */ Control_Monad_Except_Trans.bindExceptT(Effect.monadEffect));
 var liftEffect = /* #__PURE__ */ Effect_Class.liftEffect(/* #__PURE__ */ Control_Monad_Except_Trans.monadEffectExceptT(Effect_Class.monadEffectEffect));
 var debugIsSymbol = {
@@ -60,7 +73,10 @@ var fold1 = /* #__PURE__ */ fold(Options_Applicative_Builder_Internal.modMonoid)
 var $$long = /* #__PURE__ */ Options_Applicative_Builder["long"](Options_Applicative_Builder_Internal.optionFieldsHasName);
 var mempty = /* #__PURE__ */ Data_Monoid.mempty(Options_Applicative_Builder_Internal.modMonoid);
 var $$short = /* #__PURE__ */ Options_Applicative_Builder["short"](Options_Applicative_Builder_Internal.optionFieldsHasName);
+var extractChunk = /* #__PURE__ */ Options_Applicative_Help_Chunk.extractChunk(Text_PrettyPrint_Leijen.docMonoid);
+var mapFlipped2 = /* #__PURE__ */ Data_Functor.mapFlipped(Data_Functor.functorArray);
 var value = /* #__PURE__ */ Options_Applicative_Builder.value(Options_Applicative_Builder_Internal.optionFieldsHasValue);
+var metavar1 = /* #__PURE__ */ Options_Applicative_Builder.metavar(Options_Applicative_Builder_Internal.optionFieldsHasMetavar);
 var pure1 = /* #__PURE__ */ Control_Applicative.pure(/* #__PURE__ */ Control_Monad_Except_Trans.applicativeExceptT(Effect.monadEffect));
 var Wrap = function (x) {
     return x;
@@ -78,7 +94,13 @@ var AppCliArgs = function (x) {
     return x;
 };
 var readMString = {
-    readM: Options_Applicative_Builder.str
+    readM: Options_Applicative_Builder.str,
+    writeM: function (x) {
+        return x;
+    },
+    metavar: function (v) {
+        return "STRING";
+    }
 };
 var mappingWithIndexMappingFr = function (dictIsSymbol) {
     var get = Record.get(dictIsSymbol)();
@@ -90,6 +112,51 @@ var mappingWithIndexMappingFr = function (dictIsSymbol) {
                 };
             }
         };
+    };
+};
+var writeM = function (dict) {
+    return dict.writeM;
+};
+var wrapString = function (str) {
+    return function (width) {
+        var mkLine = Data_String_Common.joinWith(" ");
+        var go = function ($copy_v) {
+            return function ($copy_v1) {
+                return function ($copy_lines) {
+                    var $tco_var_v = $copy_v;
+                    var $tco_var_v1 = $copy_v1;
+                    var $tco_done = false;
+                    var $tco_result;
+                    function $tco_loop(v, v1, lines) {
+                        var v2 = function (v3) {
+                            return append(lines)([ mkLine(v1) ]);
+                        };
+                        var $144 = Data_Array.uncons(v);
+                        if ($144 instanceof Data_Maybe.Just) {
+                            var newLineCandidate = append(v1)([ $144.value0.head ]);
+                            var $145 = Data_String_CodePoints.length(mkLine(newLineCandidate)) <= width;
+                            if ($145) {
+                                $tco_var_v = $144.value0.tail;
+                                $tco_var_v1 = newLineCandidate;
+                                $copy_lines = lines;
+                                return;
+                            };
+                            $tco_var_v = $144.value0.tail;
+                            $tco_var_v1 = [ $144.value0.head ];
+                            $copy_lines = append(lines)([ mkLine(v1) ]);
+                            return;
+                        };
+                        $tco_done = true;
+                        return v2(true);
+                    };
+                    while (!$tco_done) {
+                        $tco_result = $tco_loop($tco_var_v, $tco_var_v1, $copy_lines);
+                    };
+                    return $tco_result;
+                };
+            };
+        };
+        return go(Data_String_Common.split(" ")(str))([  ])([  ]);
     };
 };
 var readM = function (dict) {
@@ -107,20 +174,36 @@ var printExpr = function (expr) {
     })(Tidy.formatExpr(defaultFormatOptions)(expr)));
 };
 var showPretty = function (dictShow) {
-    var $134 = Data_Show.show(dictShow);
-    return function ($135) {
+    var $166 = Data_Show.show(dictShow);
+    return function ($167) {
         return (function (v) {
             if (v instanceof PureScript_CST.ParseSucceeded) {
                 return printExpr(v.value0);
             };
             return "<invalid>";
-        })(PureScript_CST.parseExpr($134($135)));
+        })(PureScript_CST.parseExpr($166($167)));
     };
 };
 var printError = /* #__PURE__ */ showPretty(TsBridgeGen_Types.showAppError);
+var metavar = function (dict) {
+    return dict.metavar;
+};
 var handleErrors = function (v) {
     if (v instanceof Data_Either.Left) {
-        return quitWithError("Unexpected Error");
+        return function __do() {
+            var debug = mapFlipped(Node_Process.lookupEnv("DEBUG"))(function (v1) {
+                if (v1 instanceof Data_Maybe.Just && v1.value0 === "true") {
+                    return true;
+                };
+                return false;
+            })();
+            return quitWithError((function () {
+                if (debug) {
+                    return show(v.value0);
+                };
+                return TsBridgeGen_UIText.init.unexpectedError;
+            })())();
+        };
     };
     if (v instanceof Data_Either.Right && v.value0 instanceof Data_Either.Left) {
         return quitWithError(printError(v.value0.value0));
@@ -128,13 +211,13 @@ var handleErrors = function (v) {
     if (v instanceof Data_Either.Right && v.value0 instanceof Data_Either.Right) {
         return pure(v.value0.value0);
     };
-    throw new Error("Failed pattern match at TsBridgeGen.Config (line 195, column 16 - line 198, column 28): " + [ v.constructor.name ]);
+    throw new Error("Failed pattern match at TsBridgeGen.Config (line 242, column 16 - line 255, column 28): " + [ v.constructor.name ]);
 };
 var runInitM = function (ma) {
     return bind($$try(Control_Monad_Except_Trans.runExceptT(ma)))(handleErrors);
 };
 var getEnvVars = /* #__PURE__ */ (function () {
-    return mapFlipped(bind1(mapFlipped(liftEffect(Data_Functor.mapFlipped(Effect.functorEffect)(Node_Process.getEnv)(TypedEnv.fromEnv(TypedEnv.readEnvImpl()()(TypedEnv.readEnvFieldsCons({
+    return mapFlipped1(bind1(mapFlipped1(liftEffect(mapFlipped(Node_Process.getEnv)(TypedEnv.fromEnv(TypedEnv.readEnvImpl()()(TypedEnv.readEnvFieldsCons({
         reflectSymbol: function () {
             return "assetsDir";
         }
@@ -147,16 +230,16 @@ var getEnvVars = /* #__PURE__ */ (function () {
             return "DEBUG";
         }
     })()(TypedEnv.readEnvFieldsNil(Type_Equality.refl))()()(TypedEnv.readValueOptional(TypedEnv.parseValueBoolean)))()()(TypedEnv.readValueRequired(TypedEnv.parseValueString)))()())(Type_Proxy["Proxy"].value))))(Data_Bifunctor.lmap(Data_Bifunctor.bifunctorEither)(TsBridgeGen_Types.ErrParseEnvVars.create)))(Control_Monad_Error_Class.liftEither(Control_Monad_Except_Trans.monadThrowExceptT(Effect.monadEffect))))((function () {
-        var $136 = Safe_Coerce.coerce();
-        return function ($137) {
-            return AppEnvVars($136($137));
+        var $168 = Safe_Coerce.coerce();
+        return function ($169) {
+            return AppEnvVars($168($169));
         };
     })());
 })();
 var fromMaybeRecord = function (dictHMapWithIndex) {
-    var $138 = Heterogeneous_Mapping.hmapWithIndex(dictHMapWithIndex);
-    return function ($139) {
-        return $138(MappingFromMaybeRecord($139));
+    var $170 = Heterogeneous_Mapping.hmapWithIndex(dictHMapWithIndex);
+    return function ($171) {
+        return $170(MappingFromMaybeRecord($171));
     };
 };
 var fromMaybeRecord1 = /* #__PURE__ */ fromMaybeRecord(/* #__PURE__ */ Heterogeneous_Mapping.hmapWithIndexRecord()(/* #__PURE__ */ Heterogeneous_Mapping.mapRecordWithIndexCons(classFileIsSymbol)(/* #__PURE__ */ mappingWithIndexMappingFr(classFileIsSymbol)())(/* #__PURE__ */ Heterogeneous_Mapping.mapRecordWithIndexCons(debugIsSymbol)(/* #__PURE__ */ mappingWithIndexMappingFr(debugIsSymbol)())(/* #__PURE__ */ Heterogeneous_Mapping.mapRecordWithIndexCons(modulesFileIsSymbol)(/* #__PURE__ */ mappingWithIndexMappingFr(modulesFileIsSymbol)())(Heterogeneous_Mapping.mapRecordWithIndexNil)()())()())()()));
@@ -180,10 +263,14 @@ var mergeCfg = function (v) {
 };
 var cliOption = function (dictReadM) {
     var readM1 = readM(dictReadM);
+    var writeM1 = writeM(dictReadM);
+    var metavar2 = metavar(dictReadM);
     return function (long1) {
         return function (short1) {
             return function (help) {
-                return Options_Applicative_Builder.option(optional(readM1))(fold1([ $$long(long1), Data_Maybe.maybe(mempty)($$short)(short1), Options_Applicative_Builder.help(help), value(Data_Maybe.Nothing.value) ]));
+                return function (def) {
+                    return Options_Applicative_Builder.option(optional(readM1))(fold1([ $$long(long1), Data_Maybe.maybe(mempty)($$short)(short1), Options_Applicative_Builder.helpDoc(new Data_Maybe.Just(extractChunk(Options_Applicative_Help_Chunk.vcatChunks(append(mapFlipped2(wrapString(help)(60))(Options_Applicative_Help_Chunk.stringChunk))([ Options_Applicative_Help_Chunk.stringChunk("Defaults to " + writeM1(def)), Options_Applicative_Help_Chunk.stringChunk(" ") ]))))), value(Data_Maybe.Nothing.value), metavar1(metavar2(Type_Proxy["Proxy"].value)) ]));
+                };
             };
         };
     };
@@ -191,12 +278,12 @@ var cliOption = function (dictReadM) {
 var cliOption1 = /* #__PURE__ */ cliOption(readMString);
 var optParser = /* #__PURE__ */ (function () {
     return Data_Functor.map(Options_Applicative_Types.parserFunctor)(AppCliArgs)(Record_Extra.sequenceRecord()(Record_Extra.sequenceRecordCons(classFileIsSymbol)()(Options_Applicative_Types.parserApply)(Record_Extra.sequenceRecordSingle(modulesFileIsSymbol)()(Options_Applicative_Types.parserFunctor)()())()())({
-        classFile: cliOption1("class-file")(Data_Maybe.Nothing.value)(".."),
-        modulesFile: cliOption1("modules-file")(Data_Maybe.Nothing.value)("..")
+        classFile: cliOption1("class-file")(Data_Maybe.Nothing.value)(TsBridgeGen_UIText.cli.options.classFile)(defaults.modulesFile),
+        modulesFile: cliOption1("modules-file")(Data_Maybe.Nothing.value)(TsBridgeGen_UIText.cli.options.modulesFile)(defaults.classFile)
     }));
 })();
 var getCliArgs = /* #__PURE__ */ (function () {
-    var opts = Options_Applicative_Builder.info(Options_Applicative_Internal_Utils.apApplyFlipped(Options_Applicative_Types.parserApply)(optParser)(Options_Applicative_Extra.helper))(fold(Options_Applicative_Builder.infoModMonoid)([ Options_Applicative_Builder.fullDesc, Options_Applicative_Builder.progDesc("Print a greeting for TARGET"), Options_Applicative_Builder.header("hello - a test for purescript-optparse") ]));
+    var opts = Options_Applicative_Builder.info(Options_Applicative_Internal_Utils.apApplyFlipped(Options_Applicative_Types.parserApply)(optParser)(Options_Applicative_Extra.helper))(fold(Options_Applicative_Builder.infoModMonoid)([ Options_Applicative_Builder.fullDesc, Options_Applicative_Builder.header(TsBridgeGen_UIText.cli.header), Options_Applicative_Builder.footerDoc(Data_Newtype.unwrap()(Options_Applicative_Help_Chunk.vcatChunks([ Options_Applicative_Help_Chunk.stringChunk("Environment variables:"), Options_Applicative_Help_Chunk.tabulate([ new Data_Tuple.Tuple(extractChunk(Options_Applicative_Help_Chunk.stringChunk("DEBUG")), extractChunk(Options_Applicative_Help_Chunk.stringChunk("BOOLEAN"))) ]) ]))) ]));
     return liftEffect(Options_Applicative_Extra.execParser(opts));
 })();
 var getConfig = /* #__PURE__ */ bind1(getCliArgs)(function (cliArgs) {
