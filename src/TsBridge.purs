52330fe1a37e29ccf52c4079dad33bba4bdb8235
Janis König <janis.koenig@intcube.io>
2023-11-08 13:53:23 +0100

    Fix ImplicitQualifiedImport and UnusedExplicitImport

--- src/TsBridge.purs
+++ src/TsBridge.purs
@@ -2,10 +2,10 @@ module TsBridge
   ( module Exp
   ) where
 
-import TsBridge.Cli as Exp
-import TsBridge.Core as Exp
-import TsBridge.Monad as Exp
-import TsBridge.DefaultImpls as Exp
-import TsBridge.Types as Exp
-import TsBridge.Types.TsRecord as Exp
-import TsBridge.Types.Intersection as Exp
+import TsBridge.Cli (mkTypeGenCli) as Exp
+import TsBridge.Core (class TsBridgeBy, class TsValues, class TsValuesRL, tsBridgeBy, tsModuleFile, tsOpaqueType, tsProgram, tsTypeAlias, tsValue, tsValues, tsValuesRL) as Exp
+import TsBridge.Monad (Scope(..), TsBridgeAccum(..), TsBridgeM, getAccum, runTsBridgeM) as Exp
+import TsBridge.DefaultImpls (class TsBridgeRecord, class TsBridgeRecordRL, class TsBridgeVariant, class TsBridgeVariantEncodedFlat, class TsBridgeVariantEncodedFlatRL, class TsBridgeVariantEncodedNested, class TsBridgeVariantEncodedNestedRL, class TsBridgeVariantRL, TypeVar(..), tsBridgeArray, tsBridgeBoolean, tsBridgeChar, tsBridgeEffect, tsBridgeEither, tsBridgeFunction, tsBridgeInt, tsBridgeMaybe, tsBridgeNewtype, tsBridgeNull, tsBridgeNullable, tsBridgeNumber, tsBridgeObject, tsBridgeOneOf, tsBridgeOpaqueType, tsBridgePromise, tsBridgeRecord, tsBridgeRecordRL, tsBridgeString, tsBridgeStringLit, tsBridgeTuple, tsBridgeTypeVar, tsBridgeUndefined, tsBridgeUnit, tsBridgeVariant, tsBridgeVariantEncodedFlat, tsBridgeVariantEncodedFlatRL, tsBridgeVariantEncodedNested, tsBridgeVariantEncodedNestedRL, tsBridgeVariantRL) as Exp
+import TsBridge.Types (class CapError, class CapThrow, AppError(..), Name, PursModuleName, TsNameError(..), mapErr, mkName, mkPursModuleName, printError, printName, toTsName, unsafeName) as Exp
+import TsBridge.Types.TsRecord (class Get, class GetKey, class GetKeyRL, class GetMods, class GetModsRL, class ToRecord, class ToRecordBuilder, class TsBridgeTsRecord, class TsBridgeTsRecordRL, Mod, ModField, TsRecord, get, getMods, getModsRL, toRecord, toRecordBuilder, tsBridgeTsRecord, tsBridgeTsRecordRL) as Exp
+import TsBridge.Types.Intersection (class ToTuples, type (|&|), Intersection, fst, snd, toTuple, toTuples, tsBridgeIntersection) as Exp
